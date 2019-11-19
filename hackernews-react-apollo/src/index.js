23699db75185af9d1f649e863062732bda6219d0
Martin Bilbao <martin@bravenew.tech>
2019-11-19 07:13:14 -0300

    More Mutation and Updating the Store

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -3,28 +3,44 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants';
 
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache()
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+    connectToDevTools: true
 })
 
 ReactDOM.render(
+  <BrowserRouter>
     <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>,
-    document.getElementById('root')
-  )
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
 
   // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
